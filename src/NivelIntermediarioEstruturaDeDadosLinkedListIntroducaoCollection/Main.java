a6db38767890665f4894fdee8783f0e7b52f7dbc
Matheus Nicacio <110930694+matheussnicacio@users.noreply.github.com>
2025-04-07 10:10:41 -0300

    ED

---
+++ src/NivelIntermediarioEstruturaDeDadosLinkedListIntroducaoCollection/Main.java
@@ -0,0 +1,22 @@
+package NivelIntermediarioEstruturaDeDadosLinkedListIntroducaoCollection;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Classes
+        ArrayList<String> arrayList = new ArrayList<>();
+        LinkedList<String> linkedList = new LinkedList<>();
+        Stack<String> stack = new Stack<>();
+
+        Queue<String> queue = new LinkedList<>();
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>();
+
+
+        linkedList.add("Naruto");
+        System.out.println(linkedList);
+
+
+    }
+}
