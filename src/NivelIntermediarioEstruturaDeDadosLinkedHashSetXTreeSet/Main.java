0874988f63a6dccdf65b48fcf0670f3e1d7bfa0a
Matheus Nicacio <110930694+matheussnicacio@users.noreply.github.com>
2025-04-08 12:55:41 -0300

    TREESET

---
+++ src/NivelIntermediarioEstruturaDeDadosLinkedHashSetXTreeSet/Main.java
@@ -0,0 +1,31 @@
+package NivelIntermediarioEstruturaDeDadosLinkedHashSetXTreeSet;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        List<String> ninjas = new ArrayList<>();
+
+        ninjas.add("Naruto Uzumaki");
+        ninjas.add("Sasuke Uchiha");
+        ninjas.add("Sakura Haruno");
+        ninjas.add("Naruto Uzumaki");
+        ninjas.add("Danzo");
+        ninjas.add("Kakashi Hatake");
+        ninjas.add("Boruto Uzumaki");
+
+        System.out.println("Ninjas: " + ninjas);
+
+        Set<String> ninjasSet = new TreeSet<>();
+
+        ninjasSet.addAll(ninjas);
+
+        ninjasSet.remove("Sakura Haruno");
+
+        System.out.println("Ninjas: " + ninjasSet);
+
+    }
+
+}
